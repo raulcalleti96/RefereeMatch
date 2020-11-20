baf2297cc5a3529a9ff211281fce55ccbf29ee81
raulcalleti96 <$Elenagomez8>
2020-11-20 14:12:15 +0100

    Add complet menu. Check layout

--- app/src/main/java/com/example/refereematch/MainActivity.java
+++ app/src/main/java/com/example/refereematch/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.refereematch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int TIEMPO = 3000; //Time to launch the another activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, principalmenu.class);
+                startActivity(i);
+
+            }
+        }, TIEMPO);
     }
+
+
 }
