7595398dbea302a19d6cc8fabaeead032ca876e3
raulcalleti96 <raulcalleti96@icloud.com>
2020-11-27 03:44:07 +0100

    Lateral menu done. Is possible navigate between levels

--- app/src/main/java/com/example/refereematch/dificilActivity.java
+++ app/src/main/java/com/example/refereematch/dificilActivity.java
@@ -1,12 +1,62 @@
 package com.example.refereematch;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class dificilActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class dificilActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    //Variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbarfin;
+    Menu menu;
+    TextView textView;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mododificil);
+
+
+        //Menu
+        drawerLayout = findViewById(R.id.drawer_layout3);
+        navigationView = findViewById(R.id.nav_view);
+        toolbarfin = findViewById(R.id.toolbar2);
+
+        setSupportActionBar(toolbarfin);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbarfin, R.string.nav_draw_abrir, R.string.nav_draw_cerrar);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
--- app/src/main/java/com/example/refereematch/facilActivity.java
+++ app/src/main/java/com/example/refereematch/facilActivity.java
@@ -1,12 +1,61 @@
 package com.example.refereematch;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class facilActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+
+public class facilActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    //Variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbarfin;
+    Menu menu;
+    TextView textView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modofacil);
+
+
+        //Menu
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbarfin = findViewById(R.id.toolbar2);
+
+        setSupportActionBar(toolbarfin);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbarfin,R.string.nav_draw_abrir,R.string.nav_draw_cerrar);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else{
+
+            super.onBackPressed();
+         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return true;
     }
 }
