1a981e0b37dd61ea3150d9b16873954e01f9a150
raulcalleti96 <raulcalleti96@icloud.com>
2020-11-28 15:53:36 +0100

    Add Game

--- app/src/main/java/com/example/refereematch/facilActivity.java
+++ app/src/main/java/com/example/refereematch/facilActivity.java
@@ -1,14 +1,29 @@
 package com.example.refereematch;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -20,19 +35,27 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 
-public class facilActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class facilActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnTouchListener {
     //Variables
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbarfin;
     Menu menu;
     TextView textView;
+
+    private Tablero fondo;
+    int x, y;
+    private Casilla[][] casillas;
+    private boolean activo = true;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modofacil);
-
 
         //Menu
         drawerLayout = findViewById(R.id.drawer_layout);
@@ -46,8 +69,222 @@ public class facilActivity extends AppCompatActivity implements NavigationView.O
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout2);
+        fondo = new Tablero(this);
+        fondo.setOnTouchListener(this);
+        layout.addView(fondo);
+        casillas = new Casilla[8][8];
+        for (int f = 0; f < 8; f++) {
+            for (int c = 0; c < 8; c++) {
+                casillas[f][c] = new Casilla();
+            }
+        }
+        this.disponerBombas();
+        this.contarBombasPerimetro();
+
+
+
     }
 
+    /*======Parte de juego=====*/
+
+
+    public void presionado(View v) {
+        casillas = new Casilla[8][8];
+        for (int f = 0; f < 8; f++) {
+            for (int c = 0; c < 8; c++) {
+                casillas[f][c] = new Casilla();
+            }
+        }
+        this.disponerBombas();
+        this.contarBombasPerimetro();
+        activo = true;
+
+        fondo.invalidate();
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (activo)
+            for (int f = 0; f < 8; f++) {
+                for (int c = 0; c < 8; c++) {
+                    if (casillas[f][c].dentro((int) event.getX(),
+                            (int) event.getY())) {
+                        casillas[f][c].destapado = true;
+                        if (casillas[f][c].contenido == 80) {
+                            Toast.makeText(this, "Booooooooommmmmmmmmmmm",
+                                    Toast.LENGTH_LONG).show();
+                            activo = false;
+                        } else if (casillas[f][c].contenido == 0)
+                            recorrer(f, c);
+                        fondo.invalidate();
+                    }
+                }
+            }
+        if (gano() && activo) {
+            Toast.makeText(this, "Ganaste", Toast.LENGTH_LONG).show();
+            activo = false;
+        }
+
+        return true;
+    }
+    class Tablero  extends View {
+        public Tablero(Context context) {
+            super(context);
+        }
+
+        protected void onDraw(Canvas canvas) {
+            canvas.drawRGB(0, 0, 0);
+            int ancho = 0;
+            if (canvas.getWidth() < canvas.getHeight())
+                ancho = fondo.getWidth();
+            else
+                ancho = fondo.getHeight();
+            int anchocua = ancho / 8;
+            Paint paint = new Paint();
+            paint.setTextSize(20);
+            Paint paint2 = new Paint();
+            paint2.setTextSize(20);
+            paint2.setTypeface(Typeface.DEFAULT_BOLD);
+            paint2.setARGB(255, 0, 0, 255);
+            Paint paintlinea1 = new Paint();
+            paintlinea1.setARGB(255, 255, 255, 255);
+            int filaact = 0;
+            for (int f = 0; f < 8; f++) {
+                for (int c = 0; c < 8; c++) {
+                    casillas[f][c].fijarxy(c * anchocua, filaact, anchocua);
+                    if (casillas[f][c].destapado == false)
+                        paint.setARGB(153, 204, 204, 204);
+                    else
+                        paint.setARGB(255, 153, 153, 153);
+                    canvas.drawRect(c * anchocua, filaact, c * anchocua
+                            + anchocua - 2, filaact + anchocua - 2, paint);
+                    // linea blanca
+                    canvas.drawLine(c * anchocua, filaact, c * anchocua
+                            + anchocua, filaact, paintlinea1);
+                    canvas.drawLine(c * anchocua + anchocua - 1, filaact, c
+                                    * anchocua + anchocua - 1, filaact + anchocua,
+                            paintlinea1);
+
+                    if (casillas[f][c].contenido >= 1
+                            && casillas[f][c].contenido <= 8
+                            && casillas[f][c].destapado)
+                        canvas.drawText(
+                                String.valueOf(casillas[f][c].contenido), c
+                                        * anchocua + (anchocua / 2) - 8,
+                                filaact + anchocua / 2, paint2);
+
+                    if (casillas[f][c].contenido == 80
+                            && casillas[f][c].destapado) {
+                        Paint bomba = new Paint();
+                        bomba.setARGB(255, 255, 0, 0);
+                        canvas.drawCircle(c * anchocua + (anchocua / 2),
+                                filaact + (anchocua / 2), 8, bomba);
+                    }
+
+                }
+                filaact = filaact + anchocua;
+            }
+        }
+    }
+    private void disponerBombas() {
+        int cantidad = 8;
+        do {
+            int fila = (int) (Math.random() * 8);
+            int columna = (int) (Math.random() * 8);
+            if (casillas[fila][columna].contenido == 0) {
+                casillas[fila][columna].contenido = 80;
+                cantidad--;
+            }
+        } while (cantidad != 0);
+    }
+
+    private boolean gano() {
+        int cant = 0;
+        for (int f = 0; f < 8; f++)
+            for (int c = 0; c < 8; c++)
+                if (casillas[f][c].destapado)
+                    cant++;
+        if (cant == 56)
+            return true;
+        else
+            return false;
+    }
+
+    private void contarBombasPerimetro() {
+        for (int f = 0; f < 8; f++) {
+            for (int c = 0; c < 8; c++) {
+                if (casillas[f][c].contenido == 0) {
+                    int cant = contarCoordenada(f, c);
+                    casillas[f][c].contenido = cant;
+                }
+            }
+        }
+    }
+    int contarCoordenada(int fila, int columna) {
+        int total = 0;
+        if (fila - 1 >= 0 && columna - 1 >= 0) {
+            if (casillas[fila - 1][columna - 1].contenido == 80)
+                total++;
+        }
+        if (fila - 1 >= 0) {
+            if (casillas[fila - 1][columna].contenido == 80)
+                total++;
+        }
+        if (fila - 1 >= 0 && columna + 1 < 8) {
+            if (casillas[fila - 1][columna + 1].contenido == 80)
+                total++;
+        }
+
+        if (columna + 1 < 8) {
+            if (casillas[fila][columna + 1].contenido == 80)
+                total++;
+        }
+        if (fila + 1 < 8 && columna + 1 < 8) {
+            if (casillas[fila + 1][columna + 1].contenido == 80)
+                total++;
+        }
+
+        if (fila + 1 < 8) {
+            if (casillas[fila + 1][columna].contenido == 80)
+                total++;
+        }
+        if (fila + 1 < 8 && columna - 1 >= 0) {
+            if (casillas[fila + 1][columna - 1].contenido == 80)
+                total++;
+        }
+        if (columna - 1 >= 0) {
+            if (casillas[fila][columna - 1].contenido == 80)
+                total++;
+        }
+        return total;
+    }
+
+    private void recorrer(int fil, int col) {
+        if (fil >= 0 && fil < 8 && col >= 0 && col < 8) {
+            if (casillas[fil][col].contenido == 0) {
+                casillas[fil][col].destapado = true;
+                casillas[fil][col].contenido = 50;
+                recorrer(fil, col + 1);
+                recorrer(fil, col - 1);
+                recorrer(fil + 1, col);
+                recorrer(fil - 1, col);
+                recorrer(fil - 1, col - 1);
+                recorrer(fil - 1, col + 1);
+                recorrer(fil + 1, col + 1);
+                recorrer(fil + 1, col - 1);
+            } else if (casillas[fil][col].contenido >= 1
+                    && casillas[fil][col].contenido <= 8) {
+                casillas[fil][col].destapado = true;
+            }
+        }
+    }
+
+
+
+
+    /*======Parte de juego=====*/
 
     @Override
     public void onBackPressed() {
@@ -185,3 +422,4 @@ public class facilActivity extends AppCompatActivity implements NavigationView.O
         return true;
     }
 }
+
